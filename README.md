f196e46a5df1298dae83725d95bec4a3732655c0
Kelli Jarrell <kelli.jarrell@ninelineapparel.com>
2020-05-29 18:04:23 -0400

    Coding files

---
+++ README.md
@@ -0,0 +1,28 @@
+
+        Project Title:
+        sdfghj
+
+        Description:
+        dfghjk
+
+        Table of Contents:
+        fghjk
+
+        Installation:
+        dfghj
+
+        Usage:
+        dfghjk
+
+        License:
+        dfgh
+
+        Contributing:
+        dfghj
+
+        Tests:
+        dfghjk
+
+        GitHub info:
+        ![User Profile Picture](https://avatars1.githubusercontent.com/u/57328587?s=460&u=69967ebce07862b991fed723418ff6045bfa36ad&v=4)
+        Email: gdhyujd
---
+++ index.js
@@ -0,0 +1,103 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username?"
+        }, 
+
+        {
+            type: "input",
+            name: "title",
+            message: "What is the Project title?"
+        }, 
+        {
+            type: "input",
+            name: "description",
+            message: "What is the Project description?"
+        },
+        {
+            type: "input",
+            name: "contents",
+            message: "List the Table of Contents."
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "List your installation."
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "List your usage."
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "List your license info."
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "List your constributing factors/members."
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "List your tests."
+        },
+        {
+            type: "input",
+            name: "image",
+            message: "Supply link to your GitHub picture"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your GitHub email?"
+        }
+
+
+    ]).then(function(data) {
+
+        var HTML=`
+        Project Title:
+        ${data.title}
+
+        Description:
+        ${data.description}
+
+        Table of Contents:
+        ${data.contents}
+
+        Installation:
+        ${data.installation}
+
+        Usage:
+        ${data.usage}
+
+        License:
+        ${data.license}
+
+        Contributing:
+        ${data.contributing}
+
+        Tests:
+        ${data.test}
+
+        GitHub info:
+        ![User Profile Picture](${data.image})
+        Email: ${data.email}`;
+
+        fs.writeFile("README.md", HTML, function(err){
+
+            if (err){
+                return console.log(err);
+            }
+
+            console.log("YAYYYA!");
+        })
+    })
